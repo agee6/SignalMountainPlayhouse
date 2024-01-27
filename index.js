ff1c1a4b4bad68418c87dd7577e85dd33898492d
Austen Gee <austen.gee@vimaanrobotics.com>
2024-01-26 22:33:40 -0500

    added a bunch of stuff

--- index.js
+++ index.js
@@ -1,71 +1,51 @@
-const mainTab = document.getElementById('main');
-const historyTab = document.getElementById('history');
-const supportTab = document.getElementById('support');
-const aboutTab = document.getElementById('about');
-const mainPage = document.getElementById('main-page');
-const historyPage = documet.getElementById('history-page');
-const supportPage = document.getElementById('support-page');
-const aboutPage = document.getElementById('about-page');
+// const mainTab = document.getElementById('main');
+// const historyTab = document.getElementById('history');
+// const supportTab = document.getElementById('support');
+// const aboutTab = document.getElementById('about');
+// const mainPage = document.getElementById('main-page');
 
-function tabClickHandler(tabName) {
-    if (tabName === 'main') {
-        mainPage.classList.add('current');
-        mainPage.classList.remove('hidden');
-        mainTab.classList.add('active');
-        historyPage.classList.add('hidden');
-        supportPage.classList.add('hidden');
-        aboutPage.classList.add('hidden');
-        historyTab.classList.remove('active');
-        supportTab.classList.remove('active');
-        aboutTab.classList.remove('active');
-    } else if (tabName == 'history') {
-        mainPage.classList.remove('current');
-        mainPage.classList.add('hidden');
-        mainTab.classList.remove('active');
-        historyPage.classList.remove('hidden');
-        historyPage.classList.add('current');
-        supportPage.classList.add('hidden');
-        aboutPage.classList.add('hidden');
-        historyTab.classList.add('active');
-        supportTab.classList.remove('active');
-        aboutTab.classList.remove('active');
-    } else if (tabName === 'support') {
-        mainPage.classList.remove('current');
-        mainPage.classList.add('hidden');
-        mainTab.classList.remove('active');
-        historyPage.classList.add('hidden');
-        historyPage.classList.remove('current');
-        supportPage.classList.remove('hidden');
-        supportPage.classList.add('current');
-        aboutPage.classList.add('hidden');
-        historyTab.classList.remove('active');
-        supportTab.classList.add('active');
-        aboutTab.classList.remove('active');
-    } else if (tabName === 'about') {
-        mainPage.classList.remove('current');
-        mainPage.classList.add('hidden');
-        mainTab.classList.remove('active');
-        historyPage.classList.add('hidden');
-        historyPage.classList.remove('current');
-        supportPage.classList.add('hidden');
-        supportPage.classList.remove('current');
-        aboutPage.classList.remove('hidden');
-        aboutPage.classList.add('current');
-        historyTab.classList.remove('active');
-        supportTab.classList.remove('active');
-        aboutTab.classList.add('active');
-    }
-}
+// function tabClickHandler(tabName) {
+//     let htmlPageName = 'Public/main.html';
+//     if (tabName === 'main') {
+//         mainTab.classList.add('active');
+//         historyTab.classList.remove('active');
+//         supportTab.classList.remove('active');
+//         aboutTab.classList.remove('active');
+//     } else if (tabName == 'history') {
+//         mainTab.classList.remove('active');
+//         historyTab.classList.add('active');
+//         supportTab.classList.remove('active');
+//         aboutTab.classList.remove('active');
+//         htmlPageName = 'Public/history.html';
+//     } else if (tabName === 'support') {
+//         mainTab.classList.remove('active');
+//         historyTab.classList.remove('active');
+//         supportTab.classList.add('active');
+//         aboutTab.classList.remove('active');
+//         htmlPageName = 'Public/support.html';
+//     } else if (tabName === 'about') {
+//         mainTab.classList.remove('active');
+//         historyTab.classList.remove('active');
+//         supportTab.classList.remove('active');
+//         aboutTab.classList.add('active');
+//         htmlPageName = 'Public/about.html';
+//     }
+//     fetch(htmlPageName)
+//         .then(response => response.text())
+//         .then(html => {
+//             mainPage.innerHTML = html;
+//         });
+// }
 
-mainTab.addEventListener('click', function() {
-    tabClickHandler('main');
-});
-historyTab.addEventListener('click', function () {
-    tabClickHandler('history');
-});
-supportTab.addEventListener('click', function () {
-    tabClickHandler('support');
-});
-aboutTab.addEventListener('click', function () {
-    tabClickHandler('about');
-});
+// mainTab.addEventListener('click', function() {
+//     tabClickHandler('main');
+// });
+// historyTab.addEventListener('click', function () {
+//     tabClickHandler('history');
+// });
+// supportTab.addEventListener('click', function () {
+//     tabClickHandler('support');
+// });
+// aboutTab.addEventListener('click', function () {
+//     tabClickHandler('about');
+// });
